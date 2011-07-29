6a4e36430250420c41d7c0eb96e0e5cb1fa08c5a
Gabe Berke-Williams <gabe@thoughtbot.com>
2011-07-29 16:38:16 -0400

    Kumade.app_for should only detect Heroku remotes

--- lib/kumade.rb
+++ lib/kumade.rb
@@ -4,7 +4,7 @@ require 'kumade/thor_task'
 module Kumade
   def self.app_for(environment)
     heroku_git_url = `git remote show -n #{environment} | grep 'Push  URL' | cut -d' ' -f6`.strip
-    match = heroku_git_url.match(/:(.+)\.git$/)
+    match = heroku_git_url.match(/^git@heroku\.com:(.+)\.git$/)
     if match
       match[1]
     else
--- spec/kumade_spec.rb
+++ spec/kumade_spec.rb
@@ -3,9 +3,17 @@ require 'spec_helper'
 describe Kumade, ".app_for" do
   let(:environment){ 'staging' }
   let(:app_name){ 'staging_test' }
+  let(:not_a_heroku_env){ 'fake_heroku' }
+  let(:not_a_heroku_url){ 'git@github.com:gabebw/kumade.git' }
 
-  before { force_add_heroku_remote(environment, app_name) }
-  after  { remove_remote(environment) }
+  before do
+    force_add_heroku_remote(environment, app_name)
+    `git remote add #{not_a_heroku_env} #{not_a_heroku_url}`
+  end
+  after do
+    remove_remote(environment)
+    remove_remote(not_a_heroku_env)
+  end
 
   it "autodetects the Heroku app name" do
     Kumade.app_for(environment).should == app_name
@@ -13,6 +21,10 @@ describe Kumade, ".app_for" do
 
   it "returns an empty string if the app cannot be found" do
     Kumade.app_for('xyz').should == ""
+  end
+
+  it "returns an empty string if the remote is not a Heroku remote" do
+    Kumade.app_for(not_a_heroku_env).should == ""
   end
 end
 
