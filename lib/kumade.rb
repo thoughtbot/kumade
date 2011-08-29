d198221c6e5639a4e540d201c06d2655c5f4c8ea
Marcos Tapajos <tapajos@gmail.com>
2011-08-29 15:50:27 -0300

    Remove unused code

--- lib/kumade.rb
+++ lib/kumade.rb
@@ -17,13 +17,4 @@ module Kumade
   def self.environments
     url_remotes = `git remote`.strip.split("\n").map{|remote| [remote, `git config --get remote.#{remote}.url`.strip] }.select{|remote| remote.last =~ /^git@heroku\.com:(.+)\.git$/}.map{|remote| remote.first}
   end
-
-  def self.on_cedar!(app)
-    @cedar_apps ||= []
-    @cedar_apps << app
-  end
-
-  def self.cedar?(app)
-    @cedar_apps.include?(app)
-  end
 end
