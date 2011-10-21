61d3ee853bd40e06fd287134ff7aeb3c3c01caba
Gabe Berke-Williams <gabe@thoughtbot.com>
2011-10-21 10:28:53 -0400

    Clean up heroku spec support.

--- spec/support/heroku.rb
+++ spec/support/heroku.rb
@@ -1,33 +1,23 @@
 shared_context "when on Cedar" do
-  let(:command_line) { mock("Kumade::CommandLine instance") }
+  let(:command_line)  { mock("Kumade::CommandLine") }
+  let(:stack_command) { "bundle exec heroku stack --remote staging" }
+  let(:heroku_output) { ["aspen-mri-1.8.6", "bamboo-mri-1.9.2", "bamboo-ree-1.8.7", "* cedar (beta)"].map { |s| "  #{s}"}.join("\n") }
 
   before do
-    Kumade::CommandLine.expects(:new).
-      with("bundle exec heroku stack --remote staging").
-      returns(command_line)
+    Kumade::CommandLine.expects(:new).with(stack_command).returns(command_line)
 
-    command_line.expects(:run_or_error).returns(%{
-  aspen-mri-1.8.6
-  bamboo-mri-1.9.2
-  bamboo-ree-1.8.7
-* cedar (beta)
-})
+    command_line.expects(:run_or_error).returns(heroku_output)
   end
 end
 
 shared_context "when not on Cedar" do
-  let(:command_line) { mock("Kumade::CommandLine") }
+  let(:command_line)  { mock("Kumade::CommandLine") }
+  let(:stack_command) { "bundle exec heroku stack --remote staging" }
+  let(:heroku_output) { ["aspen-mri-1.8.6", "* bamboo-mri-1.9.2", "bamboo-ree-1.8.7", "cedar (beta)"].map {|s| "  #{s}" }.join("\n") }
 
   before do
-    Kumade::CommandLine.expects(:new).
-      with("bundle exec heroku stack --remote staging").
-      returns(command_line)
+    Kumade::CommandLine.expects(:new).with(stack_command).returns(command_line)
 
-    command_line.expects(:run_or_error).returns(%{
-  aspen-mri-1.8.6
-* bamboo-mri-1.9.2
-  bamboo-ree-1.8.7
-  cedar (beta)
-})
+    command_line.expects(:run_or_error).returns(heroku_output)
   end
 end
