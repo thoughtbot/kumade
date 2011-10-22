5ef704b22af9feaad89464c2cefeac1c2081659a
Gabe Berke-Williams <gabe@thoughtbot.com>
2011-10-22 00:16:23 -0400

    Remove unused Cucumber steps.

--- features/step_definitions/dependency_steps.rb
+++
@@ -1,3 +0,0 @@
-When /^I add "([^"]*)" from this project as a dependency$/ do |gem_name|
-  append_to_file('Gemfile', %{\ngem "#{gem_name}", :path => "#{PROJECT_ROOT}"})
-end
--- features/step_definitions/kumade_steps.rb
+++ features/step_definitions/kumade_steps.rb
@@ -1,11 +1,5 @@
 When /^I run kumade with "([^"]+)"$/ do |args|
-  When %{I run `bundle exec kumade #{args}`}
-end
-
-Given /^I have loaded "([^"]+)"$/ do |file|
-  in_current_dir do
-    load(file)
-  end
+  run_simple("bundle exec kumade #{args}")
 end
 
 Given /^a directory set up for kumade$/ do
