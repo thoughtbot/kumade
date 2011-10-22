9ea130a80f778354acb78f5da61be8d26c2f2210
Gabe Berke-Williams <gabe@thoughtbot.com>
2011-10-22 00:16:23 -0400

    Remove unused Cucumber steps.

--- features/step_definitions/bundler_steps.rb
+++ features/step_definitions/bundler_steps.rb
@@ -2,15 +2,6 @@ When /^I bundle$/ do
   run_bundler
 end
 
-When /^I rebundle$/ do
-  run_bundler
-  commit_everything_in_repo
-end
-
-Given /^an empty Gemfile$/ do
-  write_file('Gemfile', '')
-end
-
 When /^I set up the Gemfile with kumade$/ do
   add_kumade_to_gemfile
 end
